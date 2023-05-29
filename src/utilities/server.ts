d47f7fd27274c106ae7f5f9b2f48f6f0e7de0264
Your Name <you@example.com>
2023-05-29 23:24:05 +0600

    [DONE] Setup express, mongoose, dotenv, colors, cors, @types/cors, @type/express and ts node dev for server running

---
+++ src/utilities/server.ts
@@ -0,0 +1,26 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import app from "../index";
+var colors = require("colors");
+dotenv.config();
+
+import config from "../config/config";
+
+const databaseConnect = async () => {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log(colors.black.underline.bgGreen("Database is connected!"));
+
+    app.listen(config.port, () => {
+      console.log(
+        colors.bgMagenta.brightYellow(
+          `Example app listening on port ${config.port}`
+        )
+      );
+    });
+  } catch (error: any) {
+    console.log(colors.white.bgRed("Fail to DB connected!", error.message));
+  }
+};
+
+export default databaseConnect;
